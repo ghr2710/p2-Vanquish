33e7d85170db3815fbffba5aa47615af80674fa0
George Roberts <ghr2710@gmail.com>
2022-05-05 09:53:44 -0500

    added repository files

---
+++ vote-app-boot/src/main/java/com/revature/voteappboot/data/CommentRepository.java
@@ -0,0 +1,15 @@
+package com.revature.voteappboot.data;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.voteappboot.classes.User;
+import com.revature.voteappboot.classes.Post;
+import com.revature.voteappboot.classes.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+	public User findByUsername(String username);
+}
---
+++ vote-app-boot/src/main/java/com/revature/voteappboot/data/UserRepository.java
@@ -0,0 +1,13 @@
+package com.revature.voteappboot.data;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.voteappboot.classes.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	public User findByUsername(String username);
+}
