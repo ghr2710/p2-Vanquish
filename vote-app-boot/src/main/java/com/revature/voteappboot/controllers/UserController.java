7fb6780a0ccc833c6d778cd423e3032dc9a09ede
faydalimehmet <faydalimehmet@gmail.com>
2022-05-06 14:59:01 -0400

    User Repo

--- vote-app-boot/src/main/java/com/revature/voteappboot/controllers/UserController.java
+++ vote-app-boot/src/main/java/com/revature/voteappboot/controllers/UserController.java
@@ -4,30 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.voteappboot.classes.User;
 import com.revature.voteappboot.exceptions.UserAlreadyExistsException;
 import com.revature.voteappboot.services.userService;
 
-
-
 @RestController
-@RequestMapping(path="/users")
-@CrossOrigin(origins="http://localhost:4200")
+@RequestMapping(path = "/users")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
-private userService userServ;
-	
+	private userService userServ;
+
 	@Autowired
 	public UserController(userService userServ) {
 		this.userServ = userServ;
-		
+
 	}
+
 	@PostMapping
 	public ResponseEntity<User> createAcct(@RequestBody User u) {
 		try {
@@ -36,5 +34,21 @@ private userService userServ;
 		} catch (UserAlreadyExistsException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
+	}
+
+
+	//	@GetMapping(path = "/{username}")
+	//	public ResponseEntity<User> findByUsername(@PathVariable int username) {
+	//		User user = userServ.findByUsername(username);
+	//		if (user != null) {
+	//			return ResponseEntity.ok(user);
+	//		} else {
+	//			return ResponseEntity.notFound().build();
+	//		}
+	//	}
+
+	@PostMapping
+	public void deleteUser(@RequestParam String username) {
+		userService.deleteUser(username);
 	}
 }
--- vote-app-boot/src/main/java/com/revature/voteappboot/services/userService.java
+++ vote-app-boot/src/main/java/com/revature/voteappboot/services/userService.java
@@ -1,45 +1,36 @@
 package com.revature.voteappboot.services;
 
 import java.util.List;
-import java.util.Date;
 
-
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.revature.voteappboot.classes.Comment;
+import com.revature.voteappboot.classes.Post;
 import com.revature.voteappboot.classes.User;
 import com.revature.voteappboot.data.CommentRepository;
 import com.revature.voteappboot.data.PostRepository;
 import com.revature.voteappboot.data.UserRepository;
 import com.revature.voteappboot.exceptions.IncorrectCredsException;
 import com.revature.voteappboot.exceptions.UserAlreadyExistsException;
-import com.revature.voteappboot.exceptions.UserDoesNotExistException;
-
-import com.revature.voteappboot.data.UserRepository;
-import com.revature.voteappboot.data.PostRepository;
-import com.revature.voteappboot.data.CommentRepository;
-
-import com.revature.voteappboot.classes.*;
 
 @Service
 public class userService {
-	
+
 	private UserRepository userRepo;
 	private PostRepository postRepo;
 	private CommentRepository commentRepo;
-	
+
 	@Autowired
 	public userService(UserRepository userRepo, PostRepository postRepo, CommentRepository commentRepo) {
 		this.userRepo = userRepo;
 		this.postRepo = postRepo;
 		this.commentRepo = commentRepo;
 	}
-	
+
 	public User createAcct(User u) throws UserAlreadyExistsException{
 		String uName = userRepo.save(u).getUsername();
-		
+
 		if(uName != "") {
 			return u;
 		}
@@ -47,7 +38,7 @@ public class userService {
 			throw new UserAlreadyExistsException();
 		}
 	}
-	
+
 	public User logIn(String username, String password) throws IncorrectCredsException{
 		User u = new User();
 		u = userRepo.findByUsername(username);
@@ -58,15 +49,15 @@ public class userService {
 		}
 
 	}
-	
+
 	public Post createPost(String postBody, String option1, String option2) {
 		Post p = new Post();
 		p.setPostBody(postBody);
 		p.setOption1(option1);
 		p.setOption2(option2);
-		
+
 		int id = postRepo.save(p).getPostId();
-		
+
 		if(id != 0) {
 			p.setPostId(id);
 		}
@@ -74,7 +65,7 @@ public class userService {
 		return p;
 
 	}
-	
+
 	public Post getPost(int id) {
 		Post p = postRepo.getById(id);
 		return p;
@@ -84,24 +75,36 @@ public class userService {
 		List<Post> postList = postRepo.findAll();
 		return postList;
 	}
-	
+
 	public Comment createComment(Comment c) {
 		int id = commentRepo.save(c).getCommentId();
-		
+
 		if(id != 0) {
 			c.setCommentId(id);
 		}
-		
+
 		return c;
-	
-	} 
-	
+
+	}
+
 	public Comment getComment(int id) {
 		return commentRepo.getById(id);
 	}
-	
+
 	public List<Comment> getComments(int postId) {
 		return commentRepo.findById(postId);
-		
+
 	}
+
+	public static void deleteUser(String username) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public User findByUsername(int username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 }
