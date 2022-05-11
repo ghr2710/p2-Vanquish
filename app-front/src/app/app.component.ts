6ad65f97d38d46bc8d4da2cb5b9e2707c24b0e05
smaklilu90 <smaklilu90@gmail.com>
2022-05-11 10:25:32 -0500

    register new users now works with pass validation

--- app-front/src/app/app.component.ts
+++ app-front/src/app/app.component.ts
@@ -21,7 +21,8 @@ export class AppComponent {
     "password": "test"
   }
 */
-
+  passMessage = ""; //password match validation message 
+  post: any;
   async login() {
     let credentials = {
       username:(<HTMLInputElement>document.getElementById('username')).value,
@@ -47,6 +48,44 @@ export class AppComponent {
     this.loginStatus= false;  //show or hide posts by setting this var true/false 
     sessionStorage.setItem('In-Use', String(this.loginStatus));
   }
+
+
+  PassValidation() {
+  let  pass1=(<HTMLInputElement>document.getElementById('password1')).value;
+  let pass2=(<HTMLInputElement>document.getElementById('password2')).value;
+
+   if (pass1!= pass2){
+    this.passMessage = "the passwords don't match"
+   } else if (pass1== pass2)
+   this.passMessage = "registered successfully! please login above"
+   this.Register();
+  }
+
+  async Register() {
+    //let username = sessionStorage.getItem('Auth-Token');
+    let credentials = {
+     
+      username:(<HTMLInputElement>document.getElementById('username1')).value,
+      password:(<HTMLInputElement>document.getElementById('password1')).value
+    
+    };
+    let credentialJSON = JSON.stringify(credentials);
+
+    let resp = await fetch('http://localhost:8080/users',
+    {method:'POST', body:credentialJSON, headers:this.headers});
+    if (resp.status===200) {
+      this.post = await resp.json();
+    } else if (resp.status===409){
+      this.passMessage = "username already exists please choose another"
+    }
+    location.reload;
+  }
+
+
+
+
+
+
 }
 
 
